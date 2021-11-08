12a5bf8f9dec87357b00c25f2251cc0a1020a83c
Latifur Rahman <lemonbabu2@gmail.com>
2021-11-09 00:13:08 +0600

    first commit

---
+++ app/src/main/java/com/tbl/shibwhalealerts/view/ui/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.tbl.shibwhalealerts.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tbl.shibwhalealerts.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
