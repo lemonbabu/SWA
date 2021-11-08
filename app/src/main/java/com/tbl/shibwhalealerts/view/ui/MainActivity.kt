dff0975655e32baf83f74e23f8baeea73bc2d7c1
Latifur Rahman <lemonbabu2@gmail.com>
2021-11-09 02:29:05 +0600

    desgin fatch

--- app/src/main/java/com/tbl/shibwhalealerts/view/ui/MainActivity.kt
+++ app/src/main/java/com/tbl/shibwhalealerts/view/ui/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.tbl.shibwhalealerts.view.ui
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.tbl.shibwhalealerts.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnBack.visibility = View.INVISIBLE
+
+
+    }
+
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setTitle("Closing Activity")
+            .setMessage("Are you sure? Do you want to exit this app?")
+            .setPositiveButton("Yes") { dialog, which -> finish() }
+            .setNegativeButton("No", null)
+            .show()
     }
 }
